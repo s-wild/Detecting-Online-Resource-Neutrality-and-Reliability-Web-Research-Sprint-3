349ce5183d1ce653dc429292dc4df890d0c9eb9b
MGovier <merlingovier@gmail.com>
2016-03-04 15:29:24 +0000

    Node and Express set-up

--- node/app/index.js
+++ node/app/index.js
@@ -1,12 +1,16 @@
 'use strict';
+require('dotenv').config(); // Get config from .env file - inc API keys.
 
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const routes = require('./routes');
 const AnalysisMethods = require('./methods/analysis');
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+app.use(bodyParser.json()); // parse application/json
 
 const methods = {
   analysis: new AnalysisMethods()
@@ -18,8 +22,7 @@ const methods = {
 function start() {
   routes.setup(app, methods);
   app.listen(port);
-  console.log(`Express server listening on port ${port}
-                in ${app.settings.env} mode.`);
+  console.log(`Listening on port ${port} in ${app.settings.env} mode.`);
 }
 
 start();
