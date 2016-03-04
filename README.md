dbf2740ced3701fc37df683c7c731f05ea40f168
MGovier <merlingovier@gmail.com>
2016-03-04 15:29:51 +0000

    Use Readability API to extract content from article URL.

--- README.md
+++ README.md
@@ -1,18 +1,18 @@
 # WebRes Sprint 3
-up637415, up656421, up663652
+By: up637415, up656421, up663652
 
-## Chrome Extension in extension directory
+### Chrome Extension in extension/
 This performs API requests against the Node.JS server
 
-## Node.JS server
+### Node.JS server in node/
 Handles data requests against multiple API services and delivers results to extension.
 
-### Installation
+#### Installation
 Requires Node 4+.
 
 Use `npm install` from inside the node directory to bring in dependencies.
 
-### Linting
+#### Linting
 Uses Google's eslint configuration. Run `npm lint` to check.
 
 Requires [eslint](http://eslint.org/), with google and xo packages.
@@ -21,3 +21,10 @@ Requires [eslint](http://eslint.org/), with google and xo packages.
   npm install -g eslint-config-google
   npm install -g eslint-config-xo
 ```
+
+#### Config and API keys
+Requires a API keys to communicate with services:
+* (Readability Parser API)[https://www.readability.com/developers/api/parser]
+
+These keys should be placed in a `.env` file in the node folder.
+An example is provides as `.env-sample`.
--- node/app/methods/analysis.js
+++ node/app/methods/analysis.js
@@ -1,18 +1,51 @@
 'use strict';
 
+const request = require('request');
+
+const errorCodes = require('../lib/ErrorCodes');
+
 const AnalysisMethods = function() {
   /**
    * Perform analysis of URL.
-   * @param  {[type]} req [description]
-   * @param  {[type]} res [description]
+   * @param  {object} req Express request.
+   * @param  {object} res Response.
    */
   function analyzeURL(req, res) {
-    console.log(req);
-    res.send('ok');
+    const url = req.body.url;
+
+    const handleResponse = (error, response, body) => {
+      if (error || response.statusCode !== 200) {
+        res.status(errorCodes.server.code);
+        res.send(errorCodes.server.message + ': ' + error ||
+                  response.statusCode);
+        console.log('Error with Readability API Call:', error ||
+                    response.statusCode);
+      }
+      if (response.statusCode === 200) {
+        let data = analyzeContent(body);
+        res.send(data);
+      }
+    };
+    request('https://readability.com/api/content/v1/parser?' +
+      `url=${url}&token=${process.env.READABILITY_API_KEY}`, handleResponse);
   }
 
   /**
-   * Get results for a previously analyzed URL.
+   * Perform analysis on extracted URL content.
+   * @param  {JSON}   rbody   Readability parser results.
+   * @return {JSON}   data    Results of analysis.
+   */
+  function analyzeContent(rbody) {
+    const body = JSON.parse(rbody);
+    let data = {
+      title: body.title,
+      content: body.content
+    };
+    return data;
+  }
+
+  /**
+   * Get results for an analyzed URL.
    * @param  {[type]} req [description]
    * @param  {[type]} res [description]
    */
