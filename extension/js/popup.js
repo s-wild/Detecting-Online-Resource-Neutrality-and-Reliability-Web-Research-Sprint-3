47e3bbbff41f839d20a9d6c4a65e34a14ced7725
up637415 <up637415@myport.ac.uk>
2016-03-07 19:10:49 +0000

    Added activity loader functionality incase server takes a while to respond.

--- extension/js/popup.js
+++ extension/js/popup.js
@@ -7,6 +7,8 @@
 
 // DOM Content Ready function.
 document.addEventListener('DOMContentLoaded', function() {
+  hideResults();
+  activityIndicator();
 
   // Get URL contents.
   postTabURL();
@@ -43,6 +45,7 @@ function postTabURL() {
                 sentimentValue = responseValues.sentiment;
                 console.log("responseValues", sentimentValue);
                 gaugeGenerator(sentimentValue, "sentiment");
+                showResults();
             }
             else {
               console.log("error");
@@ -94,5 +97,37 @@ function gaugeGenerator(value, type){
     gauge.animationSpeed = 5; // set animation speed (32 is default value)
     gauge.set(value); // set actual value
   }
-
+}
+function activityIndicator() {
+  var opts = {
+    lines: 13, // The number of lines to draw
+    length: 14, // The length of each line
+    width: 3, // The line thickness
+    radius: 11, // The radius of the inner circle
+    scale: 1, // Scales overall size of the spinner
+    corners: 1, // Corner roundness (0..1)
+    color: '#000', // #rgb or #rrggbb or array of colors
+    opacity: 0.25, // Opacity of the lines
+    rotate: 0, // The rotation offset
+    direction: 1, // 1: clockwise, -1: counterclockwise
+    speed: 1, // Rounds per second
+    trail: 60, // Afterglow percentage
+    fps: 20, // Frames per second when using setTimeout() as a fallback for CSS
+    zIndex: 2e9, // The z-index (defaults to 2000000000)
+    className: 'spinner', // The CSS class to assign to the spinner
+    top: '70%', // Top position relative to parent
+    left: '50%', // Left position relative to parent
+    shadow: false, // Whether to render a shadow
+    hwaccel: false, // Whether to use hardware acceleration
+    position: 'absolute', // Element positioning
+  };
+  var target = document.getElementById('spinner');
+  var spinner = new Spinner(opts).spin(target);
+}
+function hideResults() {
+  document.getElementById("results").style.display = 'none';
+}
+function showResults() {
+  document.getElementById("results").style.display = 'block';
+  document.getElementById("spinner").style.display = 'none';
 }
